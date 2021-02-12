a46bb8e455164cba1fe4c5b57fd976743c8553c6
GauravKumarKarki <77915442+GauravKumarKarki@users.noreply.github.com>
2021-02-12 21:02:01 +0545

    new commit

---
+++ src/Practice/CombinationSum.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practice;
+import java.util.*;
+
+/**
+ *
+ * @author PREDATOR
+ */
+public class CombinationSum {
+    public static void main(String[]args){
+    
+    }
+    
+    public static void combine(){
+        int[]array={1,2,2,5,6,8};
+        int length=array.length;
+        int target=0;
+        
+        target*
+        
+    }
+}
