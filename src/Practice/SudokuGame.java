1f32885a3f547fcbb9e968e113e017bfdf02a7a6
GauravKumarKarki <77915442+GauravKumarKarki@users.noreply.github.com>
2021-02-12 08:52:55 +0545

    new commit

---
+++ src/Practice/SudokuGame.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practice;
+
+/**
+ *
+ * @author PREDATOR
+ */
+public class SudokuGame {
+    public static void main(String[]args){
+        game();
+    }
+    
+    public static void game(){
+        int[][] board= new int[9][9];
+        int fIndex=0;
+        int sIndex=0;
+        Boolean valid= true;
+        int singlecell=3;
+        
+//      main column and row
+        for(int main=0;main<board.length;main++){
+            
+            // for main row
+            for(int col=1;col<board.length;col++){
+                if(board[main][sIndex]==board[main][col]){
+                    System.out.println("It is not valid");
+                    
+                }else{
+                    
+                    valid=false;
+                }
+                sIndex++;
+            }
+            
+            // for main column
+            for(int row=1;row<board.length;row++){
+                if(board[fIndex][main]==board[main][row]){
+                    System.out.println("It is not valid");
+                }else{
+                    
+                    valid=false;
+                }
+                fIndex++;
+            }
+            
+        }
+        fIndex=0;
+        sIndex=0;
+        int[] boxArray=new int[9];
+
+        // after valid, now smaller 9 3x3 boxes
+        if(valid==true){
+            // for each box
+            for(int box=0;box<board.length;box++){
+                // each cell of a box
+                for(int cell=0;cell<9;cell++){
+                    // for each cell to compare
+                    for(singlecell=0;singlecell<3;singlecell++){
+                        boxArray[sIndex]=board[fIndex][singlecell];
+                        sIndex++;
+                    }
+                    fIndex++;
+                }
+                fIndex=0;
+                sIndex=0;
+                
+            
+                for(int i=0;i<boxArray.length;i++){  
+                    for(int j=0;j<2;j++){
+                        if(boxArray[i]==board[box][j]){
+                            System.out.println("It is invalid");
+                        }
+                    }
+                }
+            }
+        }
+
+  
+    }
+}
